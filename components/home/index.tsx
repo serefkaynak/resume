2064ca7ab3c5222533f6d87480a7a7e4b6807934
Şeref Kaynak <serefkynk@gmail.com>
2024-12-20 09:02:39 +0300

    fixed: modified index.tsx

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -6,19 +6,19 @@ import SignUp from "./signUp"
 import WorkHistory from "./workHistory"
 
 export default function HomeComponent() {
-  
 
-  return (
-    <div data-testid="home-component" className={'ml-4 mr-2'}>
-      <Hero />
-      {/*<ImageGallery />*/}
-      <div className="lg:flex flex-row lg:my-16 my-8">
-      <Posts />
-      <div className="flex flex-col lg:my-16 my-8">
-      <SignUp />
-      <WorkHistory />
-      </div>
-      </div>
-    </div>
-  )
+
+    return (
+        <div data-testid="home-component" className={'ml-4 mr-2'}>
+            <Hero/>
+            {/*<ImageGallery />*/}
+            <div className="lg:flex flex-row lg:my-16 my-8 justify-start">
+                <Posts/>
+                <div className="flex flex-col lg:my-16 my-8">
+                    <SignUp/>
+                    <WorkHistory/>
+                </div>
+            </div>
+        </div>
+    )
 }
