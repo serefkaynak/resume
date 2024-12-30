6b15248939c563f86ae3f2fe5771fda3cd27bffa
Şeref Kaynak <serefkynk@gmail.com>
2024-12-30 23:28:03 +0300

    fixed: improved projectHome component

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 import Hero from "./hero"
-import ImageGallery from "./imageGallery"
+//import ImageGallery from "./imageGallery"
 import Posts from "./posts"
 import SignUp from "./signUp"
 import WorkHistory from "./workHistory"
 import {projectsArr} from "@/components/projects";
-import ProjectTile from "@/components/projects/projectTile";
 import Image from "next/image";
+import ProjectHome from "@/components/projects/projectHome";
 
 export default function HomeComponent() {
 
@@ -15,27 +15,10 @@ export default function HomeComponent() {
         <div data-testid="home-component" className={'ml-4 mr-2'}>
             <Hero/>
             {/*<ImageGallery />*/}
-                <h2 className="mt-4 mb-2 font-semibold dark:text-zinc-100">Projects</h2>
+            <h2 className="mt-4 mb-2 font-semibold dark:text-zinc-100">Projects</h2>
             {projectsArr.map(i => (
                 <div key={i.id} className={'flex flex-col justify-start items-start gap-2 mb-4'}>
-                    <div className={'flex flex-row justify-start gap-2'}>
-                        <div
-                            className=" rounded-full overflow-hidden flex justify-center items-center p-1 shadow w-10 h-10">
-                            <Image
-                                className=" rounded-full overflow-hidden w-8 h-8"
-                                data-testid="project-img"
-                                src={i.imgSrc}
-                                width={50}
-                                height={50}
-                                alt={"Project preview"}
-                                title={i.title}
-                            />
-                        </div>
-                        <div className={'flex flex-col justify-start items-start gap-1'}>
-                            <h2 className="text-zinc-900 hover:text-zinc-900 dark:text-zinc-100 font-semibold">{i.title}</h2>
-                            <p className="text-zinc-600 hover:text-zinc-900 dark:text-zinc-400">{i.blurb.slice(0, 150)}...</p>
-                        </div>
-                    </div>
+                    <ProjectHome {...i}/>
                 </div>
             ))}
             <div className="lg:flex flex-row lg:my-16 my-8 justify-start">
---
+++ components/projects/projectHome.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image"
+import Link from "next/link"
+import { BsLink45Deg } from "react-icons/bs"
+import React from "react";
+
+interface ProjectHomeProps {
+  id: number
+  imgSrc: string
+  title: string
+  blurb: string
+  link: string
+}
+
+export default function ProjectHome(props: ProjectHomeProps) {
+  const { imgSrc, title, blurb, link } = props
+
+  return (
+      <div className={'flex flex-row justify-start gap-2'}>
+          <div
+              className=" rounded-full overflow-hidden flex justify-center items-center p-1 shadow w-10 h-10">
+              <Image
+                  className=" rounded-full overflow-hidden w-8 h-8"
+                  data-testid="project-img"
+                  src={imgSrc}
+                  width={50}
+                  height={50}
+                  alt={"Project preview"}
+                  title={title}
+              />
+          </div>
+          <div className={'flex flex-col justify-start items-start gap-1'}>
+              <h2 className="text-zinc-900 hover:text-zinc-900 dark:text-zinc-100 font-semibold">{title}</h2>
+              <p className="text-zinc-600 hover:text-zinc-900 dark:text-zinc-400">{blurb.slice(0, 150)}...</p>
+          </div>
+      </div>
+  )
+}
